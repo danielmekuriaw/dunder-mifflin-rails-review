6c734a0de1eba7aef64c0ad330f49b8be5cdfeff
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-06 11:24:42 -0500

    dundler mifflin rails lab DONE

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,12 @@
 class Dog < ApplicationRecord
+    has_many :employee
+
+    def self.sort_by_employee_count
+        self.all.sort_by{
+            |dog| dog.employee.count
+        }
+    end
+    #def employee_size
+    #    self.employee.all
+    #end
 end
